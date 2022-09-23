6768704e5315a9c9a091d437fc1a4e6cd4b6d259
lukstein <35562073+lukstein@users.noreply.github.com>
2022-09-23 23:36:26 +0200

    Temp. measurement based on w1 included
    The temperature reading is implemented based on 1-wire technology. This article served as tutorial. https://www.circuitbasics.com/raspberry-pi-ds18b20-temperature-sensor-tutorial/

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import time
 import RPi.GPIO as GPIO
+import os
+import glob 
+ 
+# initialize 1wire
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
 
 # initialize GPIO
 GPIO.setmode(GPIO.BCM)
@@ -9,7 +15,7 @@ p = GPIO.PWM(gpio_pin, 500)  # frequency=500Hz
 p.start(0)
 
 
-# initialize variables
+# initialize general variables
 p_h = 0.0 # (Watt) current power at house-grid-connection
 p_c = 0.0 # (Watt) current power to zero
 p_max = 2000 # (Watt) maximum power consumption
@@ -17,11 +23,38 @@ p_buffer = 100.0 # (watt) Buffer to not pull from the grid.
 t_c = 25.0 # °C current temperature
 t_max = 75.0 # °C max temp
 
+# initliaze temperature variables
+t_sensorname = "28-0000000d756b" # ID of 1-Wire temperature sensor
+base_dir = '/sys/bus/w1/devices/' # not to change
+device_file = base_dir + t_sensorname + '/w1_slave' # not to change
+
+def read_temp_raw():
+    """Reads raw output file of 1 wire protocol of the specified sensor.
+    """
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
+
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
+
 def measure_t_c():
     """Measures current temperature of boiler.
     Returns temperature or None if error."""
-    temp = input("Enter current temperature in °C:")
-    return float(temp)
+    try: 
+        temp = read_temp()
+    except:
+        temp = None
+    return temp
 
 def measure_p_h():
     """Measures power at the house-grid connection. 
